b7bdaca6bf4aa73336e19e82ee32efacad37a4f2
awchjimmy <awchjimmy@gmail.com>
2024-09-21 08:46:32 +0800

    api 問題從資料庫讀取

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from database import get_cursor
+
 app = FastAPI()
 
 # Define allowed origins
@@ -38,4 +40,22 @@ def request_question(room_id: int, question_id: int):
     "option3": "18",
     "option4": "11",
     "answer": "option1"}
+    return result
+
+@app.get("/show-question")
+def show_question(room_id: int = 7777):
+    # get current question id by room id
+    cursor = get_cursor()
+    cursor.execute('SELECT question_id FROM rooms WHERE room_id=?', [room_id])
+    question_id = cursor.fetchone()[0]
+
+    # get question text by question id
+    cursor.execute('SELECT * FROM questions WHERE question_id=?', [question_id])
+    q = cursor.fetchone()
+
+    # convert to json
+    result = {"question": q[3], "option1": q[4], "option2": q[5], "option3": q[6], "option4": q[7], "answer": q[8]}
+
+    # print(result)
+
     return result
