88fde2113d2fa608ad87dfd44186fb6e680f7303
awchjimmy <awchjimmy@gmail.com>
2024-10-14 10:05:04 +0800

    主持人選分類

---
+++ public/js/show-category.js
@@ -0,0 +1,10 @@
+/**
+ * 主持人選分類
+ */
+function setCategory(category) {
+    const ref = firebase.firestore().collection('show-category-display').doc('A89mIhkZn1Re2aIhRRDe')
+    ref.update({'current_category': category, 'current_number': 1})
+    .then(() => { console.log("Document successfully updated!"); })
+    .catch((error) => { console.error("Error updating document: ", error); })
+    return null;
+}
