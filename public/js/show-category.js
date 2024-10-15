a527889145b2f7b792a7d0646e6d463bbe5944da
awchjimmy <awchjimmy@gmail.com>
2024-10-15 17:14:02 +0800

    選分類 讀題目

--- public/js/show-category.js
+++ public/js/show-category.js
@@ -4,7 +4,12 @@
 function setCategory(category) {
     const ref = firebase.firestore().collection('show-category-display').doc('A89mIhkZn1Re2aIhRRDe')
     ref.update({'current_category': category, 'current_number': 1})
-    .then(() => { console.log("Document successfully updated!"); })
+    .then(() => {
+        console.log("Document successfully updated!");
+        // redirect
+        window.location = '/show-question.html'
+    })
     .catch((error) => { console.error("Error updating document: ", error); })
+
     return null;
 }
